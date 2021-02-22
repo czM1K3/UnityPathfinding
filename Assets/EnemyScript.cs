f51a977dc301a55800eb460d0dafe7a4948eb5e0
czM1K3 <misamadera1@gmail.com>
2021-02-22 10:01:14 +0100

    Added animation for enemy

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
@@ -12,7 +13,7 @@ public class EnemyScript : MonoBehaviour
     void Start()
     {
         NextMoves = new Queue<Vector2>();
-        InvokeRepeating("Move", 0, 0.18f);
+        InvokeRepeating("Move", 0, 0.0018f);
     }
 
     void Move()
@@ -26,18 +27,18 @@ public class EnemyScript : MonoBehaviour
             if (NextMoves.Peek().y > transform.position.x) GoTo(Vector3.right);
             else if (NextMoves.Peek().y < transform.position.x) GoTo(Vector3.left);
 
-            if (NextMoves.Peek().x == transform.position.y && NextMoves.Peek().y == transform.position.x) NextMoves.Dequeue();
+            if (NextMoves.Peek().x == Math.Round(transform.position.y, 2) && NextMoves.Peek().y == Math.Round(transform.position.x, 2)) NextMoves.Dequeue();
         }
     }
 
     void GoTo(Vector3 way)
     {
-        transform.position += way;
+        transform.position += way * 0.01f;
     }
 
     public Vector2 GetPos()
     {
-        return new Vector2(transform.position.x, transform.position.y);
+        return new Vector2((float)Math.Round(transform.position.x, 2), (float)Math.Round(transform.position.y, 2));
     }
 
     void Vlna(int[,] pole, Vector2 pos)
